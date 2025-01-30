66d0e837909c9668db288de3b483aee49d72d21b
Quillan Gee <qgee@chapman.edu>
2025-01-29 21:37:41 -0800

    Parse Unit Tests 2 of them

--- Cpsc370Final.Tests/ParseUnitTest.cs
+++ Cpsc370Final.Tests/ParseUnitTest.cs
@@ -5,7 +5,16 @@ using Cpsc370Final;
 public class ParseUnitTest
 {
     [Fact]
+    public void InitializeTest()
+    {
+        ParseTextFiles.IntializeDictionaries();
+        Assert.NotNull(ParseTextFiles.ShoeBrand);
+    }
+
+    [Fact]
     public void ParseTest()
     {
+        Dictionary<int,string> myDict = Cpsc370Final.ParseTextFiles.ParseTextFile("test1.txt");
+        Assert.Equal("turtle",myDict[10]);
     }
 }
